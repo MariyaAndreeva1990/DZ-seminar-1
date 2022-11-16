fe1b4a2aa275f349b71905ab5ba3533651e01f21
Mariya Andreeva <mashanasha_1@example.com>
2022-11-16 19:35:35 +0600

    Add the ending task

--- Independent001/Program.cs
+++ Independent001/Program.cs
@@ -9,20 +9,27 @@ if (b == a * a);
     {
         Console.WriteLine("Второе число является квадратом первого");
     }*/
-    Console.WriteLine("Введите 2 числа");
+    Console.WriteLine("Введите первое число");
     int firstnumber = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите второе число");
     int secondnumber = Convert.ToInt32(Console.ReadLine());
-        if (firstnumber == secondnumber * secondnumber)
-     {
+    if (firstnumber == secondnumber * secondnumber)
+    {
         Console.WriteLine("Первое число является квадратом второго");
     }
-        else if (secondnumber == firstnumber * firstnumber)
+    else if (secondnumber == firstnumber * firstnumber)
     {
         Console.WriteLine("Второе число является квадратом первого");
     }
-        else if (firstnumber != secondnumber * secondnumber)
+    else
+        {
+        Console.WriteLine("Ни одно из чисел не является квадратом второго");
+        }
+        /*else if (firstnumber != secondnumber * secondnumber)
         {
         Console.WriteLine("Первое число не является квадратом второго");
         }
         else if (secondnumber != firstnumber * firstnumber)
+        {
         Console.WriteLine("Второе число не является квадратом первого");
+        }*/
