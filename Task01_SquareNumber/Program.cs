96ecb9d92f3a014ab7444fa51f712977ed0f8e61
Mariya Andreeva <mashanasha_1@example.com>
2022-11-16 09:53:49 +0600

    Сonsolidated the information

---
+++ Task01_SquareNumber/Program.cs
@@ -0,0 +1,8 @@
+﻿//Convert.ToInt32(); конверт для преобразования одного значения в другое (Console.ReadLine() это строчные данные типа стринг, и для того, чтобы пользователь мог вводить числа, конвертируем стринговую строку)
+Console.Write("Введите число: ");
+string input = Console.ReadLine();// string input - ввод строки
+int number = Convert.ToInt32(input);
+ 
+int sqr = number * number;
+
+Console.WriteLine(number + "->"+ sqr); // "приклеили" пробел, "приклеили" результат и вывели в одну строку с результатом
